c4d0ca96031ccbb1e713f576673b818576e00e17
= <=>
2022-03-10 13:27:37 -0600

    added more-info, pasted js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,94 @@
 // Put your JavaScript here
+const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+const DETAIL_INFO_SELECTOR = '[data-image-role="info"]';
+const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+const HIDDEN_DETAIL_CLASS = 'hidden-detail';
+const TINY_EFFECT_CLASS = 'is-tiny';
+
+
+function setDetails(imageUrl, titleText, moreInfo) {
+    'use strict';
+    let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src', imageUrl);
+
+    let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailTitle.textContent = titleText;
+
+    let detailInfo = document.querySelector(DETAIL_INFO_SELECTOR);
+    detailInfo.textContent = moreInfo
+}
+
+function imageFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-title')
+}
+
+function infoFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-info')
+}
+
+function setDetailsFromThumb(thumbnail) {
+    'use strict';
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail), infoFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumb) {
+    'use strict';
+    thumb.addEventListener('click', function(event) {
+        event.preventDefault(); //stop browser from following link
+        setDetailsFromThumb(thumb);
+        showDetails(); //show the big detail
+    });
+}
+
+function getThumbnailsArray() {
+    'use strict';
+    let thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    let thumbnailArray = [].slice.call(thumbnails); //convert NodeList to array
+    return thumbnailArray;
+}
+
+function hideDetails() {
+    'use strict';
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+    'use strict';
+    let frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+    setTimeout(function () {
+        frame.classList.remove(TINY_EFFECT_CLASS);
+    }, 50);
+}
+
+function addKeyPressHandler() {
+    'use strict';
+    document.body.addEventListener('keyup', function(event) {
+        event.preventDefault();
+        console.log(event.key);
+    if (event.key == 'Escape') {
+        hideDetails();
+    }
+});
+}
+
+function initializeEvents() {
+    'use strict';
+    let thumbnails = getThumbnailsArray();
+    thumbnails.forEach(addThumbClickHandler);
+    addKeyPressHandler();
+}
+
+//run all functions to link thumbnails to callback
+//that will update the main detail image with thumbnail image+title
+initializeEvents();
